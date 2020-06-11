8667f9bd5f85d92b4b19de842ae4cdce42116662
Igo Saldanha <pess.igosaldanha@gmail.com>
2020-06-11 17:44:33 -0300

    Movimentação básica do e jogo de camera

---
+++ Assets/Scripts/Camera3Pessoa.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class Camera3Pessoa : MonoBehaviour
+{
+    public GameObject cabeca;
+
+    public GameObject[] posicoes;
+    public int indise = 0;
+    public float velocidadeMovimenoto = 2;
+    private RaycastHit hit;
+
+    void FixedUpdate()
+    {
+
+        transform.LookAt(cabeca.transform);
+        //Chekcar se tem colisor
+
+        if (!Physics.Linecast(cabeca.transform.position, posicoes[indise].transform.position))
+        {
+            transform.position = Vector3.Lerp(transform.position, posicoes[indise].transform.position, velocidadeMovimenoto * Time.deltaTime);
+            Debug.DrawLine(cabeca.transform.position, posicoes[indise].transform.position);
+        }
+        else if (Physics.Linecast(cabeca.transform.position, posicoes[indise].transform.position, out hit)){
+            transform.position = Vector3.Lerp(transform.position, hit.point, (velocidadeMovimenoto * 2) * Time.deltaTime);
+            Debug.DrawLine(cabeca.transform.position, hit.point);
+
+
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("v") && indise < (posicoes.Length - 1)) {
+
+            indise++;
+
+        }
+        else if (Input.GetKeyDown("v") && indise >= (posicoes.Length - 1))
+        {
+
+            indise = 0;
+
+        }
+
+
+
+    }
+
+}
+
---
+++ Assets/Scripts/PersonagenWalk.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagenWalk : MonoBehaviour
+{
+
+    public float rotacionar = 200;
+    private Animator _animator;
+    private float _andar = 0;
+
+
+    void Start()
+    {
+
+        _animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _andar = Input.GetAxis("Vertical");
+
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _andar += 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _andar = 1;
+        }
+
+
+        _animator.SetFloat("Andar", _andar);
+        this.transform.Rotate(0, (Input.GetAxis("Horizontal") * rotacionar) * Time.deltaTime, 0);
+
+    }
+}
