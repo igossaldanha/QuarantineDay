95a016fa190ce2b46152a674789123f64d81b808
Igo Saldanha <pess.igosaldanha@gmail.com>
2020-06-15 17:20:05 -0300

    Adicionado minegames projeto

---
+++ Assets/MineGames/Cartas/Cartas/Assets/Scripts/MainCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour {
+
+    [SerializeField] private SceneController controller;
+    [SerializeField] private GameObject BackC;
+
+    public void OnMouseDown()
+    {
+        if(BackC.activeSelf && controller.canReveal)
+        {
+            BackC.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void ChangeSprite(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image; //This gets the sprite renderer component and changes the property of it's sprite!
+    }
+
+    public void Unreveal()
+    {
+        BackC.SetActive(true);
+    }
+
+
+}
